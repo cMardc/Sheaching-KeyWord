d9b31a598ff699af309fd6f3a134a1b70d678d90
cMardc <130239955+cMardc@users.noreply.github.com>
2023-04-24 16:50:32 +0400

    Add files via upload

---
+++ Shearching KeyWord.cpp
@@ -0,0 +1,39 @@
+﻿#include "Shearching KeyWord.h"
+using namespace std;
+namespace fs = filesystem;
+#pragma comment(lib, "Winmm.lib")
+
+
+int main(int argc, char* argv[])
+{
+	string targetPath, keyWord, name, text;
+	cout << "Enter Target Path : ";
+	getline(cin, targetPath);
+	cout << "Enter Keyword : ";
+	getline(cin, keyWord);
+	bool open;
+	for (const auto& target : fs::directory_iterator(targetPath))
+	{
+		if (ext(target.path().generic_string()) == ".txt")
+		{
+			name = getName(target.path().generic_string());
+			ifstream file(target.path());
+			open = file.is_open();
+			if (!open)
+			{
+				cout << "One File Couldn't Be Opened";
+			}
+			else
+			{
+				getline(file, text);
+				const char* found = strstr(lower(text).c_str(), lower(keyWord).c_str());
+				const char* found2 = strstr(lower(name).c_str(), lower(keyWord).c_str());
+				if (found or found2)
+				{
+					cout << "\n\n\n" << name << "\n\n" << text;
+				}
+			}
+		}
+	}
+	return 0;
+}
---
+++ Shearching KeyWord.h
@@ -0,0 +1,102 @@
+﻿#pragma once
+
+//Libraries Used
+#include <iostream> //Input/Output Stream For Console
+#include <string>   //For String
+#include <fstream>  //File Stream 
+#include <vector>   //Changable Index Structure
+#include <Windows.h>//For System Commands
+#include <algorithm>//Easy Sorting For Arrays/Vectors ...
+#include <iomanip>  //For Outputting With Given Precision
+#include <cmath>    //Easy Math Functions
+#include <stdio.h>  //Standart Input/Output For Given Types
+#include <conio.h>  //For Getting Key Presses
+#include <ctime>    //For Gettig Real Time
+#include <filesystem>
+#include <mmsystem.h>
+#include <Mmsystem.h>
+#include <mciapi.h>
+#pragma comment(lib, "Winmm.lib")
+
+using namespace std;//Standart Namespace
+namespace fs = filesystem;
+
+
+string ext(string path)
+{
+	ifstream CFile(path); //Checking/Reading File
+	bool opened = CFile.is_open(); //bool For Checking
+	if (opened) // If Available
+	{
+		string ext = ""; //Set Extension To Nothing
+		size_t size = path.size(); //Get Size Of Path
+		size_t lastPos = size;     //Get LastPos(Will Be Used Later)
+		char letter;               //Letter For Given Position
+		bool ThereIsDot = false;   //Check If There Is Format
+		for (size_t i = 0; i < size; i++) //Loop To Extract Chars
+		{
+			letter = path[i]; //Get Char On Given Index
+			if (letter == '.') //If Symbol Is Dot 
+			{
+				lastPos = i; //Starting Index Of Format
+				ThereIsDot = true; //There Is Format
+			}
+		}
+		if (ThereIsDot) //If Format Is Given
+		{
+			for (size_t i = lastPos; i < size; i++) //Loop For Format Chars
+			{
+				letter = path[i]; //Get Char On Given Index
+				ext += letter; //Get Format 
+			}
+			return ext; //Give Extension Of File
+		}
+		else
+		{
+			return "ERROR"; //Give ERROR If There Is No Extension
+		}
+	}
+	else
+	{
+		return "ERROR"; //Give ERROR If File Not Opened
+	}
+}
+
+string lower(string input)
+{
+	size_t size = input.size();//Get Size Of String
+	char symbol; // Char For Every Index
+	string ans = ""; //Set Answer to Empty String
+	for (size_t i = 0; i < size; i++) //Loop To Extract String
+	{
+		symbol = tolower(input[i]); //Set Char To It's Lowercase
+		ans += symbol; //Add It To Answer String
+	}
+	return ans;//Give Answer
+}
+
+string getName(string target)
+{
+	size_t size = target.size();
+	char symbol;
+	size_t lastPos = size;
+	string result;
+	for (size_t i = 0; i < size; i++)
+	{
+		symbol = target[i];
+		if (symbol == '/')
+		{
+			lastPos = i;
+		}
+	}
+	for (size_t i = lastPos + 1; i < size; i++)
+	{
+		symbol = target[i];
+		if (symbol == '.')
+		{
+			break;
+		}
+		result += symbol;
+	}
+	return result;
+}
